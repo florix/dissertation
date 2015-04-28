ccf5d93eab73fd578a4f95952b8d68bd135237a0
florix <florix@users.noreply.github.com>
2015-04-28 12:22:55 +0200

    Optimization: Deleted unused variable char *ptr, 28/04/2015

--- source_arduino/ethernet_prova_2/ethernet_prova_2.ino
+++ source_arduino/ethernet_prova_2/ethernet_prova_2.ino
@@ -13,10 +13,9 @@
 
 
 /* GLOBAL VARIABLES */
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xFA, 0x0F };    // Arduino MAC adress
+byte   mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xFA, 0x0F };    // Arduino MAC adress
 char    buffer[50];		// buffer used to store the first line of the request
-char *ptr;		// char pointer used to access to the element
-EthernetClient client;		// The client
+EthernetClient   client;		// The client
 uint8_t    NumberOfSpace = 0;			// Number of space in the request
 uint8_t    i = 0;						// index for the string buffer
 
@@ -24,12 +23,12 @@ uint8_t    i = 0;						// index for the string buffer
 EthernetServer server(80);		// server initialization
 
 
-/***********************************/
+/***************************************/
 /*	Name: Setup                    */
 /*	Params: Void                   */
 /*	Return: Void                   */
 /*	Description: This is the setup */
-/***********************************/
+/***************************************/
 void setup() {
   Serial.begin(9600);
    
@@ -43,13 +42,13 @@ void setup() {
 }
 
 
-/**************************************/
+/******************************************/
 /*	Name: Loop                        */
 /*	Params: Void                      */
 /*	Return: Void                      */
 /*	Description: This is the main     */
 /*	loop, it will be executed forever */
-/**************************************/
+/******************************************/
 void loop() {
   // listen for incoming clients
   
@@ -109,13 +108,13 @@ void loop() {
 
 
 
-/***************************************/
+/*******************************************/
 /*	Name: HTTP_response_GET1           */
 /*	Params: Void                       */
 /*	Return: Void                       */
 /*	Description: This is the routine   */
 /*	called whenever the path is /prova */
-/***************************************/
+/*******************************************/
 void HTTP_response_GET1() 
 {
     client.println("HTTP/1.1 200 OK");
@@ -132,13 +131,13 @@ void HTTP_response_GET1()
 
 
 
-/****************************************/
+/********************************************/
 /*	Name: HTTP_response_GET2            */
 /*	Params: Void                        */
 /*	Return: Void                        */
 /*	Description: This is the routine    */
 /*	called whenever the path is /prova1 */
-/****************************************/
+/********************************************/
 void HTTP_response_GET2() 
 {
     client.println("HTTP/1.1 200 OK");
@@ -155,13 +154,13 @@ void HTTP_response_GET2()
 
 
 
-/****************************************************/
+/********************************************************/
 /*	Name: HTTP_BAD_REQUEST                          */
 /*	Params: Void                                    */
 /*	Return: Void                                    */
 /*	Description: This is the routine                */
 /*	called whenever the path doesn't match anything */
-/****************************************************/
+/********************************************************/
 void HTTP_BAD_REQUEST() 
 {
     client.println("HTTP/1.1 400 BAD REQUEST");
