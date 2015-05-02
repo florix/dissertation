1693ebbeac6609fcf524659f869fcad697c7656d
florix <florix@users.noreply.github.com>
2015-05-02 15:43:22 +0200

    Eliminata variabile globale ethernet client e aggiunta come parametro nele varie funzioni

--- source_arduino/ethernet_prova_2/ethernet_prova_2.ino
+++ source_arduino/ethernet_prova_2/ethernet_prova_2.ino
@@ -5,21 +5,20 @@
 /* DHCP server, is able to decode 2 kinds of HTTP requests */
 /***********************************************************/
 
+// Note: stdint.h has been already included because it is used by Arduino.h
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <string.h>
-#include <stdint.h>
+
 
 
 
 /* GLOBAL VARIABLES */
 byte   mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xFA, 0x0F };    // Arduino MAC adress
-char    buffer[50];		// buffer used to store the first line of the request
-EthernetClient   client;		// The client
+char    buffer[50];		// buffer used to store the first line of the request	
 uint8_t    NumberOfSpace = 0;			// Number of space in the request
 uint8_t    i = 0;						// index for the string buffer
-
-
 EthernetServer server(80);		// server initialization
 
 
@@ -29,16 +28,13 @@ EthernetServer server(80);		// server initialization
 /*	Return: Void                   */
 /*	Description: This is the setup */
 /***************************************/
-void setup() {
-  Serial.begin(9600);
-   
-
-
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+void setup()
+{
+    Serial.begin(9600);
+    Ethernet.begin(mac);    // start the Ethernet connection 
+    server.begin();         // and the server
+    Serial.print("server is at ");
+    Serial.println(Ethernet.localIP());
 }
 
 
@@ -49,61 +45,61 @@ void setup() {
 /*	Description: This is the main     */
 /*	loop, it will be executed forever */
 /******************************************/
-void loop() {
-  // listen for incoming clients
-  
-  client = server.available();
-  
-  if (client) {
+void loop() 
+{
+    // listen for incoming clients
     
-    Serial.println("new client");
+    EthernetClient   client = server.available();      //the client
     
-    /* acquisition HTTP request */
-    while (client.connected()) {
+    if (client) {
       
-       if(client.available()) {
+      Serial.println("new client");
+      
+      
+      /* acquisition HTTP request */
+      while (client.connected()) {
         
-        char c = client.read();
-        if (c == ' ' && NumberOfSpace == 1) {
-          buffer[i] = 0;
-          goto decode;
-        }
-        else if (c == ' ') {
+         if(client.available()) {
           
-          buffer[i] = c;
-          i ++;
-          NumberOfSpace ++;
+          char c = client.read();
+          if (c == ' ' && NumberOfSpace == 1) {
+            buffer[i] = 0;
+            goto decode;
+          }
+          else if (c == ' ') {
+            buffer[i] = c;
+            i ++;
+            NumberOfSpace ++;
+          }
+          else {
+            buffer[i] = c;
+            i ++;
+          }
         }
-        else {
-          buffer[i] = c;
-          i ++;
-        }
+        
       }
       
+      decode:
+      Serial.println(buffer);
+  
+      
+      // decode the HTTP request from the longest possible path
+      // in this case the longest could be GET /prova1
+      if (strncmp(buffer, "GET /prova1", 11) == 0) {
+        HTTP_response_GET2(client);
+      } else if (strncmp(buffer, "GET /prova", 10) == 0) {
+        HTTP_response_GET1(client);
+      } else {
+        HTTP_BAD_REQUEST(client);
+      }
+      
+      
+      delay(1);         // give the web browser time to receive the data
+      client.stop();    // close the connection
+      NumberOfSpace = 0;
+      i = 0;              // reset for the next client
+      Serial.println("client disonnected");
     }
-    
-    decode:
-    Serial.println(buffer);
-
-    
-    // decode the HTTP request from the longest possible path
-    // in this case the longest could be GET /prova1
-    if (strncmp(buffer, "GET /prova1", 11) == 0) {
-      HTTP_response_GET2();
-    } else if (strncmp(buffer, "GET /prova", 10) == 0) {
-      HTTP_response_GET1();
-    } else {
-      HTTP_BAD_REQUEST();
-    }
-    
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    NumberOfSpace = 0;			// reset for the next connection
-    i = 0;		// reset for the next connection
-    Serial.println("client disonnected");
-  }
 }
 
 
@@ -115,18 +111,18 @@ void loop() {
 /*	Description: This is the routine   */
 /*	called whenever the path is /prova */
 /*******************************************/
-void HTTP_response_GET1() 
+void HTTP_response_GET1(EthernetClient  & client) 
 {
     client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connnection: close");
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<body>");
-          client.println("Ciao");
-          client.println("</body>"); 
-          client.println("</html>");
+    client.println("Content-Type: text/html");
+    client.println("Connnection: close");
+    client.println();
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");
+    client.println("<body>");
+    client.println("Ciao");
+    client.println("</body>"); 
+    client.println("</html>");
 }
 
 
@@ -138,18 +134,18 @@ void HTTP_response_GET1()
 /*	Description: This is the routine    */
 /*	called whenever the path is /prova1 */
 /********************************************/
-void HTTP_response_GET2() 
+void HTTP_response_GET2(EthernetClient  & client) 
 {
     client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connnection: close");
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<body>");
-          client.println("Ciao Ciao");
-          client.println("</body>");
-          client.println("</html>");
+    client.println("Content-Type: text/html");
+    client.println("Connnection: close");
+    client.println();
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");
+    client.println("<body>");
+    client.println("Ciao Ciao");
+    client.println("</body>");
+    client.println("</html>");
 }
 
 
@@ -161,7 +157,7 @@ void HTTP_response_GET2()
 /*	Description: This is the routine                */
 /*	called whenever the path doesn't match anything */
 /********************************************************/
-void HTTP_BAD_REQUEST() 
+void HTTP_BAD_REQUEST(EthernetClient  & client) 
 {
     client.println("HTTP/1.1 400 BAD REQUEST");
           client.println("Content-Type: text/html");
